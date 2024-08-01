2ec8dc6f414a92d8d98dedef7af1e43c522af84c
long.nguyen.van.T03 <longnv.main@gmail.com>
2024-08-01 09:57:23 +0700

    update Example

--- Example/App.tsx
+++ Example/App.tsx
@@ -2,20 +2,27 @@ import * as React from 'react';
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {scanNfc} from 'react-native-read-nfc-passport';
 
+let called = false;
+
 const MyComponent = () => {
   // documentNumber: Last 9 digits of CCCD
   // dateOfBirth: yymmdd
   // dateOfExpiry: yymmdd
   const onReadNfc = async () => {
-    try {
-      const data = await scanNfc({
-        documentNumber: 'xxxxxxxxx',
-        dateOfBirth: 'xxxxxx',
-        dateOfExpiry: 'xxxxxx',
-      });
-      console.log('onReadNfc', data);
-    } catch (error) {
-      console.log('onReadNfc error', error);
+    if (!called) {
+      called = true;
+      try {
+        const data = await scanNfc({
+          documentNumber: 'xxxxxxxxx',
+          dateOfBirth: 'xxxxxx',
+          dateOfExpiry: 'xxxxxx',
+        });
+        console.log('onReadNfc', data);
+        called = false;
+      } catch (error) {
+        called = false;
+        console.log('onReadNfc error', error);
+      }
     }
   };
 
